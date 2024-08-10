b04b37088ab955d03293e4fa40d0b4e20835bb23
waqar98mushtaq <ali8622waqar@gmail.com>
2024-08-10 01:30:55 -0700

    work on navbar

--- src/components.js/header.js
+++ src/components.js/header.js
@@ -1,24 +1,29 @@
 import { Link } from 'react-router-dom';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faAddressBook, faBlog, faFile, faHouse, faUser } from '@fortawesome/free-solid-svg-icons';
+
+
 
 export default function AppHeader() {
   return (
     <div>
-      <Navbar className='navbar' expand="lg">
+      <Navbar expand="lg" className='navbar'>
         <Container>
           <Navbar.Brand className='title' href="#home">My Portfolio</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Collapse >
             <Nav className='nav'>
-              <Nav.Link as={Link} to="/" >Home</Nav.Link>
-              <Nav.Link as={Link} to="/about" >About</Nav.Link>
-              <Nav.Link as={Link} to="/blog" >Blog</Nav.Link>
-              <Nav.Link as={Link} to="/resume" >Resume</Nav.Link>
-              <Nav.Link as={Link} to="/contact" >Contact</Nav.Link>
+              <Nav.Link as={Link} to="/" ><FontAwesomeIcon icon={faHouse}/>Home</Nav.Link>
+              <Nav.Link as={Link} to="/about" ><FontAwesomeIcon icon={faUser}/>About</Nav.Link>
+              <Nav.Link as={Link} to="/blog" ><FontAwesomeIcon icon= {faBlog} />Blog</Nav.Link>
+              <Nav.Link as={Link} to="/resume" ><FontAwesomeIcon icon= {faFile} />Resume</Nav.Link>
+              <Nav.Link as={Link} to="/contact" ><FontAwesomeIcon icon= {faAddressBook} />Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    </div>
+      </div>
   );
 }
